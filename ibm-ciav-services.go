9d2c8ea7d7d01f2cffa9b3d59b339f2b5e798214
PushpalathaHiremath <puhirema@in.ibm.com>
2016-09-08 12:11:05 +0530

    Update ibm-ciav-services.go

--- ibm-ciav-services.go
+++ ibm-ciav-services.go
@@ -6,38 +6,26 @@ Licensed under the IBM India Pvt Ltd, Version 1.0 (the "License");
 package main
 
 import (
-	"errors"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"strconv"
-//	"github.com/hyperledger/fabric/vendor/github.com/op/go-logging"
 )
-
-//var myLogger = logging.MustGetLogger("example")
 
 type ServicesChaincode struct {
 }
 
 func (t *ServicesChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-//	myLogger.Debug("Init Code")
-//	myLogger.Debugf("Init Code 1")
-//	myLogger.Info("Init Code 2")
-//	myLogger.Notice("Init Code 3")
-//      myLogger.Warning("Init Code 4")
-//       myLogger.Error("Init Code 5")
-//       myLogger.Critical("Init Code 6")
-
 	err := stub.PutState("counter", []byte("0"))
 	return nil, err
 }
 
-
 func (t *ServicesChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 
-	 if function != "increment" {
-	 	return nil, errors.New("Invalid invoke function name. Expecting \"increment\"")
-	 }
+	if function != "increment" {
+		return nil, errors.New("Invalid invoke function name. Expecting \"increment\"")
+	}
 	val, err := stub.ReadCertAttribute("position")
 	fmt.Printf("Position => %v error %v \n", string(val), err)
 	isOk, err := stub.VerifyAttribute("position", []byte("Software Engineer")) // Here the ABAC API is called to verify the attribute, just if the value is verified the counter will be incremented.
@@ -62,13 +50,13 @@ func (t *ServicesChaincode) Invoke(stub *shim.ChaincodeStub, function string, ar
 }
 
 /*
- 		Get Customer record by customer id or PAN number
+	Get Customer record by customer id or PAN number
 */
 func (t *ServicesChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if function == "read" {
 		return read(stub, args)
-	}else {
-	 	return readAttr(stub, args)
+	} else {
+		return readAttr(stub, args)
 	}
 	return nil, errors.New("Invalid query function name. Expecting \"read\"")
 }
@@ -80,10 +68,10 @@ func readAttr(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 		return nil, err
 	}
 	jsonResp := "{ " +
-					"Attribute Name  01: "+ string(attrVal1) +
-					"Attribute Value  01 : "+ strconv.FormatBool(isPresent) +
+		"Attribute Name  01: " + string(attrVal1) +
+		"Attribute Value  01 : " + strconv.FormatBool(isPresent) +
 
-				 "}"
+		"}"
 	fmt.Printf("Query Response:%s\n", jsonResp)
 
 	bytes, err := json.Marshal(jsonResp)
@@ -93,16 +81,8 @@ func readAttr(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	return bytes, nil
 }
 
-
 func read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 	var err error
-
-	// val, err := stub.ReadCertAttribute("position")
-	// isOk, err := stub.VerifyAttribute("position", []byte("Software Engineer")) // Here the ABAC API is called to verify the attribute, just if the value is verified the counter will be incremented.
-	// if err != nil {
-	// 	return nil, err
-	// }
-
 	// Get the state from the ledger
 	Avalbytes, err := stub.GetState("counter")
 	if err != nil {
@@ -115,11 +95,8 @@ func read(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
 		return nil, errors.New(jsonResp)
 	}
 
-	jsonResp := "{\"Name\":\"counter\",\"Amount\":\"" + string(Avalbytes) +
-							"\"}"
-
-							// "Attribute " + string(val) +
-							// "Attr Value " + strconv.FormatBool(isOk) +
+	jsonResp := "{\"counter\" : " + string(Avalbytes) +
+		"\"}"
 	fmt.Printf("Query Response:%s\n", jsonResp)
 
 	bytes, err := json.Marshal(jsonResp)
